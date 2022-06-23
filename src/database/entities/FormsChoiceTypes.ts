fca60d30d9144fd11eb4da989af3b50a647363e4
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-06-22 22:53:32 -0300

    add(forms): Adiciona a tabela de escolhas.

--- src/database/entities/FormsChoiceTypes.ts
+++ src/database/entities/FormsChoiceTypes.ts
@@ -5,14 +5,14 @@ import {
   OneToMany,
   JoinColumn
 } from "typeorm";
-// import FormsChoices from "./FormsChoices";
+import FormsChoices from "./FormsChoices";
 
 @Entity({ name: 'forms__choice_types' })
 export default class FormsChoiceTypes extends BaseEntity {
   @Column({ name: 'cod', nullable: false, unique: true, length: 3 })
   cod: string;
 
-  // @OneToMany(() => FormsChoices, choice => choice.id, { nullable: true, cascade: true, onDelete: 'NO ACTION', onUpdate: 'CASCADE' })
-  // @JoinColumn({ name: 'choices' })
-  // choices: FormsChoices[]
+  @OneToMany(() => FormsChoices, choice => choice.id, { nullable: true, cascade: true, onDelete: 'NO ACTION', onUpdate: 'CASCADE' })
+  @JoinColumn({ name: 'choices' })
+  choices: FormsChoices[]
 }
---
+++ src/database/entities/FormsChoices.ts
@@ -0,0 +1,27 @@
+import BaseEntity from "./BaseEntity";
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  OneToMany
+} from "typeorm";
+import FormsChoiceTypes from "./FormsChoiceTypes";
+// import FormsQuestions from "./FormsQuestions";
+
+@Entity({ name: 'forms__choices' })
+export default class FormsChoices extends BaseEntity {
+  @Column({ name: 'items', nullable: false, unique: false })
+  items: string; //TODO verificar tipo
+
+  @Column({ name: 'answer', nullable: false, unique: false, length: 256 })
+  answer: string;
+
+  @ManyToOne(() => FormsChoiceTypes, { nullable: false, cascade: true, onDelete: 'RESTRICT', onUpdate: 'CASCADE' })
+  @JoinColumn({ name: 'forms__choice_types_id' })
+  typeId: FormsChoiceTypes;
+
+  // @OneToMany(() => FormsQuestions, question => question.id, { nullable: true, cascade: true, onDelete: 'NO ACTION', onUpdate: 'CASCADE' })
+  // @JoinColumn({ name: 'questions' })
+  // questions: FormsQuestions[]
+}
